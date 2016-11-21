9ad792c594f47aa92f0fdcb07d2ed95e5e04921d
Kristjan Rang <mail@rang.ee>
2016-11-21 17:22:04 +0200

    Initial

---
+++ app/scripts.babel/options.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+'use strict';
+
+var Options = React.createClass({
+  render: function() {    
+    return (
+      <div>
+        <h1>Test</h1>
+      </div>
+    );
+  }
+});
+
+// React.render(<Options />, document.getElementById('accessgranted'));
